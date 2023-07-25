ff90cf824b5725c689dfda96ab52021ad7688d90
Vhahangwele07 <140509423+Vhahangwele07@users.noreply.github.com>
2023-07-25 15:24:12 +0200

    Add files via upload

---
+++ NewClassPractise.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut.ui;
+
+import java.awt.Color;
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author VHAHANGWELE
+ */
+public class NewClassPractise extends JFrame{
+
+    public NewClassPractise()  {
+        setTitle("Vhahangwele Mutheiwana");
+        setSize(500, 600);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.PINK);
+        setVisible(true);
+        
+    }
+  
+    
+    
+    
+}
